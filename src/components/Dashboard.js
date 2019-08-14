72c0bc90a3e39b9bf1c4baec3e8b0ef8816fac1e
spiritxhx <spiritxhx@gmail.com>
2019-08-14 14:02:13 -0400

    create a loading component for loading page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
