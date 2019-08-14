0abafc336681b8dfc10a024311deee49299e406d
spiritxhx <spiritxhx@gmail.com>
2019-08-14 14:08:28 -0400

    create the panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,11 +3,34 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      loading: false
     };
   }
 
